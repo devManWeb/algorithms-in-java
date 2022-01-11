315e097b6e089b5ccf6f253c62a6fb7ea2b5abb5
Leonardo Bissaro <43416629+devManWeb@users.noreply.github.com>
2022-01-11 10:00:50 +0100

    Add files via upload

---
+++ insertionSort.java
@@ -0,0 +1,85 @@
+public class insertionSort{
+
+    /**
+     * @param array of ints to sort with insertion sort
+     * @param start of the unsorted part
+     * @return sorted array of ints
+     */
+    public static int[] sortInts(int[] array, int start){
+        
+        if(start == array.length || array.length == 1){
+            return array;
+        }
+
+        int tempValue = 0;
+        int counter = start;
+
+        while(counter > 0){
+            if(array[counter-1] > array[counter]){
+                tempValue = array[counter-1];
+                array[counter - 1] = array[counter];
+                array[counter] = tempValue;
+            }
+            counter--;
+        }
+        start++;
+        return sortInts(array,start);
+    }
+
+    /**
+     * @param array of strings to sort with insertion sort
+     * @param start of the unsorted part
+     * @return sorted array of strings
+     */
+    public static String[] sortStrings(String array[], int start){
+        
+        if(start == array.length || array.length == 1){
+            return array;
+        }
+
+        String tempString = "";
+        int counter = start;
+
+        while(counter > 0){
+            if(array[counter-1].compareTo(array[counter]) > 0){
+                tempString = array[counter-1];
+                array[counter - 1] = array[counter];
+                array[counter] = tempString;
+            }
+            counter--;
+        }
+        start++;
+        return sortStrings(array,start);
+    }
+
+    public static void main(String[] args){
+        
+    int[] values = {9,-514,3,2,343,681,0,58,2};  
+        System.out.println("Before Insertion Sort");  
+
+        for(int i=0; i < values.length; i++){  
+            System.out.print(values[i]+" ");  
+        }  
+        System.out.println();  
+        values = sortInts(values,0);
+        System.out.println("After Insertion Sort");  
+        for(int i=0; i < values.length; i++){  
+            System.out.print(values[i]+" ");  
+        }  
+
+        System.out.println("\n");  
+        String[] letters = {"hello","house","car","plane","bond","internet"};  
+        System.out.println("Before Insertion Sort");  
+
+        for(int i=0; i < letters.length; i++){  
+            System.out.print(letters[i]+" ");  
+        }  
+        System.out.println();  
+        letters = sortStrings(letters,0);
+        System.out.println("After Insertion Sort");  
+        for(int i=0; i < letters.length; i++){  
+            System.out.print(letters[i]+" ");  
+        }  
+
+    }
+}
